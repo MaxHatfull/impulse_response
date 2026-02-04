36fdb5778c340feb416083c6879dce77de1c3617
Max Hatfull <max.h@beam.org>
2026-02-04 21:31:44 +0000

    Add rect raycasts

--- impulse_response/raycast/rect_target.rb
+++ impulse_response/raycast/rect_target.rb
@@ -1,4 +1,54 @@
 module Raycast
   class RectTarget < Target
+    serialize :center, :width, :height
+
+    def test(ray)
+      half_width = @width / 2.0
+      half_height = @height / 2.0
+
+      min_x = @center[0] - half_width
+      max_x = @center[0] + half_width
+      min_y = @center[1] - half_height
+      max_y = @center[1] + half_height
+
+      # t refers to units along the ray
+
+      if ray.direction[0] != 0
+        t_min_x = (min_x - ray.start_point[0]) / ray.direction[0]
+        t_max_x = (max_x - ray.start_point[0]) / ray.direction[0]
+        t_min_x, t_max_x = t_max_x, t_min_x if t_min_x > t_max_x
+      else
+        return nil if ray.start_point[0] < min_x || ray.start_point[0] > max_x
+        t_min_x = -Float::INFINITY
+        t_max_x = Float::INFINITY
+      end
+
+      if ray.direction[1] != 0
+        t_min_y = (min_y - ray.start_point[1]) / ray.direction[1]
+        t_max_y = (max_y - ray.start_point[1]) / ray.direction[1]
+        t_min_y, t_max_y = t_max_y, t_min_y if t_min_y > t_max_y
+      else
+        return nil if ray.start_point[1] < min_y || ray.start_point[1] > max_y
+        t_min_y = -Float::INFINITY
+        t_max_y = Float::INFINITY
+      end
+
+      t_enter = [t_min_x, t_min_y].max
+      t_exit = [t_max_x, t_max_y].min
+
+      return nil if t_enter > t_exit
+      return nil if t_enter < 0
+      return nil if t_enter > ray.length
+
+      point = ray.start_point + ray.direction * t_enter
+
+      normal = if t_enter == t_min_x
+        Vector[ray.direction[0] > 0 ? -1 : 1, 0]
+      else
+        Vector[0, ray.direction[1] > 0 ? -1 : 1]
+      end
+
+      Hit.new(target: self, point: point, distance: t_enter, normal: normal)
+    end
   end
 end
---
+++ spec/raycast/rect_target_spec.rb
@@ -0,0 +1,95 @@
+require "spec_helper"
+require_relative "../../impulse_response/loader"
+
+RSpec.describe Raycast::RectTarget do
+  describe "#test" do
+    it "returns nil when the ray misses the rectangle" do
+      rect = Raycast::RectTarget.create(center: Vector[0, 0], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[5, 5], direction: Vector[1, 0], length: 10)
+
+      expect(rect.test(ray)).to be_nil
+    end
+
+    it "returns a hit when the ray hits the rectangle head on" do
+      rect = Raycast::RectTarget.create(center: Vector[5, 0], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, 0], direction: Vector[1, 0], length: 10)
+
+      hit = rect.test(ray)
+
+      expect(hit).to be_a(Raycast::Hit)
+      expect(hit.target).to eq(rect)
+      expect(hit.point).to eq(Vector[4, 0])
+      expect(hit.distance).to eq(4)
+      expect(hit.normal).to eq(Vector[-1, 0])
+    end
+
+    it "returns nil when the ray is too short to reach the rectangle" do
+      rect = Raycast::RectTarget.create(center: Vector[10, 0], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, 0], direction: Vector[1, 0], length: 5)
+
+      expect(rect.test(ray)).to be_nil
+    end
+
+    it "returns nil when the ray points away from the rectangle" do
+      rect = Raycast::RectTarget.create(center: Vector[-5, 0], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, 0], direction: Vector[1, 0], length: 10)
+
+      expect(rect.test(ray)).to be_nil
+    end
+
+    it "returns a hit when the ray hits at an angle" do
+      rect = Raycast::RectTarget.create(center: Vector[5, 5], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, 0], direction: Vector[1, 1], length: 20)
+
+      hit = rect.test(ray)
+
+      expect(hit).to be_a(Raycast::Hit)
+      expect(hit.target).to eq(rect)
+      expect(hit.point[0]).to be_within(0.001).of(4)
+      expect(hit.point[1]).to be_within(0.001).of(4)
+      expect(hit.normal).to eq(Vector[-1, 0]).or eq(Vector[0, -1])
+    end
+
+    it "returns nil when the ray starts inside the rectangle" do
+      rect = Raycast::RectTarget.create(center: Vector[0, 0], width: 4, height: 4)
+      ray = Raycast::Ray.new(start_point: Vector[0, 0], direction: Vector[1, 0], length: 10)
+
+      expect(rect.test(ray)).to be_nil
+    end
+
+    it "returns a hit when the ray hits the edge of the rectangle" do
+      rect = Raycast::RectTarget.create(center: Vector[5, 1], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, 0], direction: Vector[1, 0], length: 10)
+
+      hit = rect.test(ray)
+
+      expect(hit).to be_a(Raycast::Hit)
+      expect(hit.point[0]).to be_within(0.001).of(4)
+      expect(hit.point[1]).to be_within(0.001).of(0)
+    end
+
+    it "returns a hit with correct normal when hitting from above" do
+      rect = Raycast::RectTarget.create(center: Vector[0, 0], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, 5], direction: Vector[0, -1], length: 10)
+
+      hit = rect.test(ray)
+
+      expect(hit).to be_a(Raycast::Hit)
+      expect(hit.point).to eq(Vector[0, 1])
+      expect(hit.distance).to eq(4)
+      expect(hit.normal).to eq(Vector[0, 1])
+    end
+
+    it "returns a hit with correct normal when hitting from below" do
+      rect = Raycast::RectTarget.create(center: Vector[0, 0], width: 2, height: 2)
+      ray = Raycast::Ray.new(start_point: Vector[0, -5], direction: Vector[0, 1], length: 10)
+
+      hit = rect.test(ray)
+
+      expect(hit).to be_a(Raycast::Hit)
+      expect(hit.point).to eq(Vector[0, -1])
+      expect(hit.distance).to eq(4)
+      expect(hit.normal).to eq(Vector[0, -1])
+    end
+  end
+end
