ff123b823b19764796b14a0bbc058f094edc8ad8
Max Hatfull <max.h@beam.org>
2026-02-05 19:50:20 +0000

    add ssao

--- impulse_response/main.rb
+++ impulse_response/main.rb
@@ -3,6 +3,10 @@ require_relative "loader"
 Engine.start do
   Engine::Cursor.disable
 
+  Rendering::PostProcessingEffect.add_all([
+    Rendering::PostProcessingEffect.ssao(kernel_size: 8, radius: 1.0, bias: 0.025, power: 1.0)
+  ])
+
   Player.instance.spawn
   Scenery.instance.create_ground
   Map.instance.load_level("impulse_response/levels/01.csv")
