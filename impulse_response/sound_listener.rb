4b385934a3ba32dfddc342a5088df6314e155976
Max Hatfull <max.h@beam.org>
2026-02-08 20:21:53 +0000

    Left right hits

--- impulse_response/sound_listener.rb
+++ impulse_response/sound_listener.rb
@@ -10,18 +10,27 @@ class SoundListener < Engine::Component
 
   MAX_VOLUME = 1
   BAR_WIDTH = 40
-  MAX_BOUNCES = 5
+  MAX_BOUNCES = 7
 
   def update(delta_time)
     puts "======="
     @sound_hits.each do |_source, hits|
       visible_hits = hits.select { |hit| coming_towards_listener?(hit) && has_line_of_sight?(hit) }
-      grouped = visible_hits.group_by(&:total_bounces)
-      (0..MAX_BOUNCES).each do |bounces|
-        bounce_hits = grouped[bounces] || []
-        vol = bounce_hits.sum { |hit| hit_volume(hit) }
-        puts "#{bounces}: #{volume_bar(vol)}"
-      end
+      left_hits, right_hits = visible_hits.partition { |hit| hit_from_left?(hit) }
+
+      puts "LEFT:"
+      print_volume_by_bounces(left_hits)
+      puts "RIGHT:"
+      print_volume_by_bounces(right_hits)
+    end
+  end
+
+  def print_volume_by_bounces(hits)
+    grouped = hits.group_by(&:total_bounces)
+    (0..MAX_BOUNCES).each do |bounces|
+      bounce_hits = grouped[bounces] || []
+      vol = bounce_hits.sum { |hit| hit_volume(hit) }
+      puts "  #{bounces}: #{volume_bar(vol)}"
     end
   end
 
@@ -33,6 +42,16 @@ class SoundListener < Engine::Component
 
   def source_volume(hits)
     hits.select { |hit| has_line_of_sight?(hit) }.sum { |hit| hit_volume(hit) }
+  end
+
+  def hit_from_left?(hit)
+    listener_pos = Vector[game_object.pos[0], game_object.pos[2]]
+    forward = game_object.forward
+    forward_2d = Vector[forward[0], forward[2]]
+    hit_pos = hit.raycast_hit.entry_point
+    to_hit = hit_pos - listener_pos
+    # 2D cross product: negative = left, positive = right
+    forward_2d[0] * to_hit[1] - forward_2d[1] * to_hit[0] < 0
   end
 
   def coming_towards_listener?(hit)
@@ -57,7 +76,7 @@ class SoundListener < Engine::Component
 
   def hit_volume(hit)
     direct = [hit.travel_distance / SOUND_RANGE, 1.0].max
-    hit.beam_strength * (1.0 / (direct ** 2)) * (BOUNCE_LOSS ** hit.total_bounces)
+    hit.beam_strength * (1.0 / direct) * (BOUNCE_LOSS ** hit.total_bounces)
   end
 
   def volume_bar(volume)
